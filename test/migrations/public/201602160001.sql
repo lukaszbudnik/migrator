d61f4924aea4f4166de2aaeb8cf8001bfc1a368b
Łukasz Budnik <lukasz.budnik@gmail.com>
2016-02-18 00:05:13 +0100

    added support for MySQL

--- test/migrations/public/201602160001.sql
+++ test/migrations/public/201602160001.sql
@@ -1,3 +1,6 @@
-create table {schema}.config (id integer, key varchar(100), value varchar(100));
-
-alter table {schema}.config add constraint pk_id primary key (id);
+create table {schema}.config (
+  id integer,
+  k varchar(100),
+  v varchar(100),
+  primary key (id)
+);
