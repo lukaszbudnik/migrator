65552b1fc95a878cb0a1822f15c7642ffbe69355
Łukasz Budnik <lukasz.budnik@gmail.com>
2016-07-31 20:03:35 +0200

    forgot to remove skipping DB tests

--- coverage.sh
+++ coverage.sh
@@ -14,9 +14,6 @@ do
   if [[ "main" == "$package" ]]; then
     continue
   fi
-  if [[ "db" == "$package" ]]; then
-    continue
-  fi
   go test -cover -coverprofile=coverage-$package.txt ./$package
   cat coverage-$package.txt | sed '/^mode/d' >> coverage-all.txt
 done
