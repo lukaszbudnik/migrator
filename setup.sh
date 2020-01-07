ea49c77c5f0e4416503453c49bb3aa8e340f307c
Łukasz Budnik <lukasz.budnik@gmail.com>
2020-01-07 14:28:39 +0100

    added explicit gin get to make sure validator package is fetched before other packages are

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 # this is for dockerhub failing on fetching packages from gopkg.in
 # travis resolves this by 3 retries so adapting 3 retries here as well
 for i in {1..3}; do
+  # explicit gin get makes sure validator package is fetched before other packages are fetched
+  go get -v github.com/gin-gonic/gin
   go get -t -v ./... && break || sleep 15;
 done
